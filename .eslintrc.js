ee6a688bf34c2996c3af69561a139bf2f3902ce6
Paulo Amosse Cuambe <37635729+paulocuambe@users.noreply.github.com>
2021-03-23 10:06:53 +0200

    fix linting issue

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,13 +10,16 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
+    ],
   },
   overrides: [
     {
-      files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)",
-      ],
+      files: ["**/__tests__/*.{j,t}s?(x)", "**/tests/unit/**/*.spec.{j,t}s?(x)"],
       env: {
         jest: true,
       },
