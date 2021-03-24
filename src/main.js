f79721682d4bdc5b1853f0befe0671a88fb9da97
Paulo Amosse Cuambe <37635729+paulocuambe@users.noreply.github.com>
2021-03-24 17:29:50 +0200

    add font assets

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from "vue";
+import "./assets/reset.css";
+import "./assets/main.css";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
