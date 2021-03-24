40c5bebe5f35ea298af8452ff899c0bbaffe83de
Paulo Amosse Cuambe <37635729+paulocuambe@users.noreply.github.com>
2021-03-24 13:30:00 +0200

    feat: add new todo

--- src/components/todo/TodoListItem.vue
+++ src/components/todo/TodoListItem.vue
@@ -1,11 +1,23 @@
 <template>
-  <div v-if="todoList.length > 0">
-    <todo-item v-for="todo in todoList" :key="todo.id" :todo="todo" />
+  <form @submit.prevent="handleSubmit()">
+    <input type="text" name="new_todo" v-model="newTodo" />
+    <button type="submit" @click.prevent="handleSubmit()" id="submitTodo">Add</button>
+  </form>
+
+  <div v-if="errors.length > 0" id="todoErrors">
+    <p v-for="(error, index) in errors" :key="index">{{ error }}</p>
   </div>
+
+  <div v-if="todos.length > 0">
+    <todo-item v-for="todo in todos" :key="todo.id" :todo="todo" />
+  </div>
+
   <h2 v-else class="todo-list__empty-message">Congrats. You've completed all your tasks.</h2>
 </template>
 
 <script>
+import { reactive, toRefs } from "vue";
+
 import TodoItem from "./TodoItem.vue";
 
 export default {
@@ -16,6 +28,39 @@ export default {
     },
   },
   components: { TodoItem },
-  setup() {},
+  setup(props) {
+    const state = reactive({
+      newTodo: "",
+      todos: props.todoList,
+      errors: [],
+    });
+
+    const validateInput = () => {
+      state.errors = [];
+      if (state.todos.some((todo) => todo.text === state.newTodo)) {
+        state.errors.push("That item is already on the list");
+      }
+      if (!state.newTodo) {
+        state.errors.push("The field must not be empty");
+      }
+    };
+
+    const handleSubmit = () => {
+      validateInput();
+      if (state.errors.length === 0) {
+        state.todos.push({
+          id: new Date().getTime(),
+          text: state.newTodo,
+          completed: false,
+        });
+        state.newTodo = "";
+      }
+    };
+
+    return {
+      ...toRefs(state),
+      handleSubmit,
+    };
+  },
 };
 </script>
--- tests/unit/TodoListItem.spec.js
+++ tests/unit/TodoListItem.spec.js
@@ -2,7 +2,7 @@ import { mount } from "@vue/test-utils";
 import TodoListItem from "@/components/todo/TodoListItem.vue";
 
 describe("TodoList.vue", () => {
-  it("should render a list of todos", () => {
+  it("renders a list of todos", () => {
     const wrapper = mount(TodoListItem, {
       props: {
         todoList: [
@@ -16,7 +16,7 @@ describe("TodoList.vue", () => {
     expect(wrapper.findAll(".todo")).toHaveLength(3);
   });
 
-  it("should render notice message when todoList is empty", () => {
+  it("renders notice message when todoList is empty", () => {
     const wrapper = mount(TodoListItem, {
       props: {
         todoList: [],
@@ -24,5 +24,50 @@ describe("TodoList.vue", () => {
     });
 
     expect(wrapper.find(".todo-list__empty-message").text()).toContain("Congrats. You've completed all your tasks.");
+  });
+
+  it("adds a new todo and cleans input", async () => {
+    const wrapper = mount(TodoListItem, {
+      props: {
+        todoList: [],
+      },
+    });
+
+    await wrapper.get('[name="new_todo"]').setValue("Write a blog post");
+    await wrapper.get("#submitTodo").trigger("click");
+
+    expect(wrapper.get('[name="new_todo"]').element.value).toBe("");
+    expect(wrapper.findAll(".todo")).toHaveLength(1);
+  });
+
+  it("shows error message when adding empty to do", async () => {
+    const wrapper = mount(TodoListItem, {
+      props: {
+        todoList: [],
+      },
+    });
+
+    await wrapper.get('[name="new_todo"]').setValue("");
+    await wrapper.get("#submitTodo").trigger("click");
+
+    expect(wrapper.find("#todoErrors").text()).toContain("The field must not be empty");
+    expect(wrapper.findAll(".todo")).toHaveLength(0);
+  });
+
+  it("shows error message when adding repeated to do", async () => {
+    const wrapper = mount(TodoListItem, {
+      props: {
+        todoList: [],
+      },
+    });
+
+    await wrapper.get('[name="new_todo"]').setValue("Learn Vue");
+    await wrapper.get("#submitTodo").trigger("click");
+
+    await wrapper.get('[name="new_todo"]').setValue("Learn Vue");
+    await wrapper.get("#submitTodo").trigger("click");
+
+    expect(wrapper.find("#todoErrors").text()).toContain("That item is already on the list");
+    expect(wrapper.findAll(".todo")).toHaveLength(1);
   });
 });
