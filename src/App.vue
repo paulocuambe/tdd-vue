d5d16c11f5e05aeaeccb60eb2cc1f9d081a39b1d
Paulo Amosse Cuambe <37635729+paulocuambe@users.noreply.github.com>
2021-03-30 13:25:37 +0200

    wip: styling

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <router-link to="/">Home</router-link>
-  <router-link :to="{ name: 'TodoApp' }">Todo App</router-link>
-  <router-view />
+  <main class="container">
+    <router-link to="/">Home</router-link>
+    <router-link :to="{ name: 'TodoApp' }">Todo App</router-link>
+    <router-view />
+  </main>
 </template>
